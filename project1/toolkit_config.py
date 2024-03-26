9b7ae91c8ab760d9829b971b5120fe127f2b03be
TaylorZhang123 <zhangtaylor6@gmail.com>
2024-03-26 12:34:21 +1100

    Initial commit

---
+++ project1/toolkit_config.py
@@ -0,0 +1,10 @@
+'''toolkit_config.py
+Project configuration file
+**Do not submit**
+
+'''
+
+import os
+
+PRJDIR = r'C:\Users\Lenovo\codes\pycham\tmp\toolkit\toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
